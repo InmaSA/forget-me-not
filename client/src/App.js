e3af8654a5c58c2f79360829f97618426a446212
Inma <inmanewmail@gmail.com>
2019-11-13 11:57:20 +0100

    first steps to prepare server and client folders

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a Forget me not</h1>
+    </div>
+  );
+}
+
+export default App;
