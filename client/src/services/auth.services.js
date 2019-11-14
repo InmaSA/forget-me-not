1893e685da101d72918cb259ad46ae7ef64bb43d
Inma <inmanewmail@gmail.com>
2019-11-14 09:48:07 +0100

    added: auth services, redux provider, store and reducer(pending configuration)

---
+++ client/src/services/auth.services.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export default class AuthServices {
+  constructor() {
+
+      this.service = axios.create({
+        baseURL: `${process.env.REACT_APP_URL}`,
+        withCredentials: true
+      })
+  }
+
+  signup = (username, password) => this.service.post('signup', { username, password })
+  login = (username, password) => this.service.post('login', { username, password })
+  logout = () => this.service.post('logout')
+  loggedin = () => this.service.get('loggedin')
+
+  
+}  
